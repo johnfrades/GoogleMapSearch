a89fdc6bce93c63d6bf4ac3e4fac6428c2c380cd
johnfrades <johnfrades@gmail.com>
2016-10-28 23:11:05 +1300

    googol maps

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var foundMap = '';
+
+var firstD = '';
+var secondD = '';
+
+app.get('/', function(req, res){
+	res.render('maps.ejs');
+});
+
+app.get('/searchmap', function(req, res){
+	res.render('searchMap.ejs', {
+		foundMap: foundMap,
+		firstD: firstD,
+		secondD: secondD
+	});
+});
+
+
+app.get('/searchmapdirection', function(req, res){
+	res.render('searchmapDirection.ejs', {
+		firstD: firstD,
+		secondD: secondD
+	});
+});
+
+
+app.post('/searchmap', function(req, res){
+	foundMap = req.body.searchmap;
+	res.redirect('/searchmap')
+});
+
+
+app.post('/searchmapdirection', function(req, res){
+	firstD = req.body.searchmapfrom;
+	secondD = req.body.searchmapto;
+	res.redirect('/searchmapdirection')
+});
+
+
+app.listen('3000', function(req, res){
+	console.log("Server started at PORT 3000");
+});
